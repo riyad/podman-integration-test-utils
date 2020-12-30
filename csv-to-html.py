179d8cdbc8b3d90decd8c6b5c0b0c1f6d20e4a82
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2020-12-30 22:14:15 +0100

    Fix indentation

--- csv-to-html.py
+++ csv-to-html.py
@@ -10,27 +10,23 @@ import jinja2
 
 
 def main():
-  csv_file_name = sys.argv[1]
-  with open(csv_file_name, 'rt') as f:
-    csv_reader = csv.DictReader(f)
+    csv_file_name = sys.argv[1]
+    with open(csv_file_name, 'rt') as f:
+        csv_reader = csv.DictReader(f)
 
-    all_tests = list(csv_reader)
+        all_tests = list(csv_reader)
 
+    grouped_by_tests = defaultdict(list)
+    for test_name, tests in groupby(all_tests, lambda t: "{}::{}::{}".format(t['test_file'], t['test_class'], t['test_method'])):
+        grouped_by_tests[test_name].extend(list(tests))
 
+    test_names = list(grouped_by_tests.keys())
+    grouped_by_tests[test_names[0]] = sorted(grouped_by_tests[test_names[0]], key=itemgetter('podman_version', 'commit_date', 'commit_id'))
 
-  grouped_by_tests = defaultdict(list)
-  for test_name, tests in groupby(all_tests, lambda t: t.get('file_class_test')):
-    grouped_by_tests[test_name].extend(list(tests))
-
-  test_names = list(sorted(grouped_by_tests.keys()))
-  grouped_by_tests[test_names[0]] = sorted(grouped_by_tests[test_names[0]], key=itemgetter('podman_version', 'commit_date', 'commit_id'))
-
-
-
-  env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
-  template = env.get_template('template.html.j2')
-  print(template.render(test_names=test_names, tests=grouped_by_tests))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+    template = env.get_template('template.html.j2')
+    print(template.render(test_names=test_names, tests=grouped_by_tests))
 
 
 if __name__ == '__main__':
-  main()
+    main()
