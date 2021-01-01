649816be5d711a920a71d6d8a15e6145d3ecac04
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2021-01-01 21:22:35 +0100

    Allow reading CSV from STDIN

--- csv-to-html.py
+++ csv-to-html.py
@@ -10,11 +10,20 @@ import jinja2
 
 
 def main():
-    csv_file_name = sys.argv[1]
-    with open(csv_file_name, 'rt') as f:
-        csv_reader = csv.DictReader(f)
+    if len(sys.argv) == 2:
+        csv_file_name = sys.argv[1]
+    else:
+        csv_file_name = None
 
+    if csv_file_name is not None:
+        with open(csv_file_name, 'rt') as f:
+            csv_reader = csv.DictReader(f)
+            all_tests = list(csv_reader)
+    elif not sys.stdin.isatty():
+        csv_reader = csv.DictReader(sys.stdin)
         all_tests = list(csv_reader)
+    else:
+        exit(1)
 
     grouped_by_tests = defaultdict(list)
     for test_name, tests in groupby(all_tests, lambda t: "{}::{}::{}".format(t['test_file'], t['test_class'], t['test_method'])):
