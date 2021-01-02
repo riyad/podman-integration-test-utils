f0428ecde594a217f486f11861e4d5b82eefff6e
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2021-01-02 13:20:38 +0100

    Allow for sessions with limited tests

--- csv-to-html.py
+++ csv-to-html.py
@@ -25,30 +25,28 @@ def main():
     else:
         exit(1)
 
-    tests_by_name = defaultdict(list)
+    tests_by_name_and_session = defaultdict(lambda: defaultdict(defaultdict))
     test_names = set()
     test_sessions = set()
     test_summaries_by_session = defaultdict(lambda: defaultdict(int))
     for test_name, tests in groupby(all_tests, lambda t: t['_test_name']):
         tests = list(tests)
-        tests_by_name[test_name].extend(tests)
         test_names.add(test_name)
 
         for test in tests:
             test_sessions.add(test['_test_session'])
+            tests_by_name_and_session[test_name][test['_test_session']] = test
             test_summaries_by_session[test['_test_session']][test['result']] += 1
 
     test_names = sorted(list(test_names))
     test_sessions = sorted(list(test_sessions))
-    for test_name in test_names:
-        tests_by_name[test_name] = sorted(tests_by_name[test_name], key=itemgetter('_test_session'))
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
     template = env.get_template('template.html.j2')
     print(template.render(
         test_names=test_names,
         test_sessions=test_sessions,
-        tests_by_name=tests_by_name,
+        tests_by_name_and_session=tests_by_name_and_session,
         test_summaries_by_session=test_summaries_by_session,
     ))
 
