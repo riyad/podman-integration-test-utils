43869a441d37b4b89a7594eb795d50fe54d929c6
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2021-02-03 21:14:08 +0100

    Add scripts to setup docker-py tests and run test suites

---
+++ run_test_suite.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# Author: Riyad Preukschas <riyad@informatik.uni-bremen.de>
+# License: Mozilla Public License 2.0
+# SPDX-License-Identifier: MPL-2.0
+#
+# Builds Podman at <PODMAN_COMMIT_ID> and runs the docker-py
+
+
+set -euo pipefail
+
+readonly PODMAN_REPO_PATH=~/src/podman
+readonly PODMAN_SOCKET_PATH="unix:${PODMAN_REPO_PATH}/docker-py-test.sock"
+readonly DOCKER_PY_REPO_PATH=~/src/docker-py
+readonly DOCKER_PY_VIRTUALENV_PATH="${DOCKER_PY_REPO_PATH}/venv"
+readonly DOCKER_PY_LOGS_PATH="$(dirname "$0")/logs"
+
+export DOCKER_HOST="${PODMAN_SOCKET_PATH}"
+
+
+function usage() {
+  echo "Usage: $0 <PODMAN_COMMIT_ID> [<PYTEST_ARGS ...>]"
+  echo "       Builds Podman at <PODMAN_COMMIT_ID> starts an API server and runs"
+  echo "       the docker-py integration tests against it."
+}
+
+
+function main() {
+  if [[ $# -eq 0 ]]; then
+    usage
+    exit 1
+  fi
+
+  local OPT_PODMAN_COMMIT_ID=$1
+  shift
+
+  cd "${PODMAN_REPO_PATH}"
+  git checkout "${OPT_PODMAN_COMMIT_ID}"
+
+  local PODMAN_COMMIT_ID="$(git log -1 --format=%H)"
+  local PODMAN_COMMIT_DATE="$(git log -1 --format=%cI)"
+  local LOG_BASE_NAME="${DOCKER_PY_LOGS_PATH}/podman_integration_${PODMAN_COMMIT_DATE}_${PODMAN_COMMIT_ID}"
+
+  make -j "$(nproc)" podman
+
+  ./bin/podman info --format json > "${LOG_BASE_NAME}.podman-info.json"
+
+  # TODO: put behind flag
+  ./bin/podman rm -a
+
+  # TODO: put behind flag
+  killall -r 'podman.*'
+
+  # somehow the output can't be redirected
+  ./bin/podman system service -t 0 "${PODMAN_SOCKET_PATH}" 2>&1 > "${LOG_BASE_NAME}.server.log" &
+
+  cd "${DOCKER_PY_REPO_PATH}"
+  source "${DOCKER_PY_VIRTUALENV_PATH}/bin/activate"
+  pytest -c pytest_podman_apiv2.ini "${@}" | tee "${LOG_BASE_NAME}.pytest.log"
+
+  # kill backgrounbd jobs (i.e. podman server)
+  kill "$(jobs -p)"
+}
+
+
+main "${@}"
---
+++ setup_test_suite.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Author: Riyad Preukschas <riyad@informatik.uni-bremen.de>
+# License: Mozilla Public License 2.0
+# SPDX-License-Identifier: MPL-2.0
+#
+# Sets up docker-py so we can run the tests.
+
+set -euo pipefail
+
+readonly PODMAN_REPO_PATH=~/src/podman
+readonly DOCKER_PY_REPO_PATH=~/src/docker-py
+readonly DOCKER_PY_VIRTUALENV_PATH="${DOCKER_PY_REPO_PATH}/venv"
+readonly DOCKER_PY_LOGS_PATH="${DOCKER_PY_REPO_PATH}/logs"
+
+
+function main() {
+  if [[ ! -d "${DOCKER_PY_REPO_PATH}" ]]; then
+    echo "docker-py repo missing. Please clone it first."
+    exit 1
+  fi
+  cd "${DOCKER_PY_REPO_PATH}"
+
+  mkdir -p "${DOCKER_PY_LOGS_PATH}"
+
+  # create custom pytest config
+  # see https://docs.pytest.org/en/stable/reference.html#ini-options-ref
+  # see https://github.com/containers/podman/issues/5386#issuecomment-755298337
+  if [[ ! -e pytest_podman_apiv2.ini ]]; then
+    cat > pytest_podman_apiv2.ini <<-EOF
+[pytest]
+addopts =
+    --tb=short
+    -rxs
+    --cache-clear
+    -v
+    -k 'not SwarmTest and not NodesTest and not ServiceTest and not SecretAPITest and not TestStore and not ConfigAPITest and not test_create_network_attachable and not test_create_network_ingress and not test_create_inspect_network_with_scope'
+
+junit_suite_name = docker-py
+junit_family = xunit2
+
+testpaths =
+    tests/integration/
+EOF
+  fi
+
+  # create virtualenv
+  python3 -m venv "${DOCKER_PY_VIRTUALENV_PATH}"
+  source "${DOCKER_PY_VIRTUALENV_PATH}/bin/activate"
+
+  # install requiements in virtualenv
+  pip install -U -r requirements.txt
+  pip install -U -r test-requirements.txt
+  # make sure at least pytest is up-to-date
+  pip install -U pytest
+}
+
+
+main "${@}"
