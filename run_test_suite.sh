08ec3b9a0edef83cc983e67b24b896ff38c7fa84
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2021-02-06 12:55:01 +0100

    Disentangle suite tagging and checkout of a specific Podman version

--- run_test_suite.sh
+++ run_test_suite.sh
@@ -4,8 +4,8 @@
 # License: Mozilla Public License 2.0
 # SPDX-License-Identifier: MPL-2.0
 #
-# Builds Podman at <PODMAN_VERSION> and runs the docker-py integration tests
-# against it.
+# Builds Podman starts an API server and runs the docker-py integration tests
+# against it. It collects engine information, test and server logs for each run.
 
 
 set -euo pipefail
@@ -23,14 +23,15 @@ export DOCKER_HOST="${PODMAN_SOCKET_PATH}"
 
 
 function usage() {
-  echo "Usage: $0 <PODMAN_VERSION> [OPTIONS] [<PYTEST_ARGS ...>]"
-  echo "       Builds Podman at <PODMAN_VERSION> starts an API server and runs"
-  echo "       the docker-py integration tests against it."
+  echo "Usage: $0 <SUITE_TAG> [OPTIONS] [<PYTEST_ARGS ...>]"
+  echo "       Builds Podman starts an API server and runs the docker-py"
+  echo "       integration tests against it. It collects engine information,"
+  echo "       test and server logs for each run."
   echo ""
   echo "OPTIONS"
+  echo " -c,--checkout \"<BRANCH>\"  Podman branch/commit to checkout. Set this to"
+  echo "                             '' to prevent doing acheckout. (default: master)"
   echo " -m,--message \"<MESSAGE>\"  Message/comment/note to attach to this test run"
-  echo "              --no-checkout  Do not do a `git checkout` in the Podman repo"
-  echo "                             containers before running the tests"
   echo "               --no-cleanup  Do not stop and remove Podman and Buildah"
   echo "                             containers before running the tests"
   echo "                  --no-kill  Do not kill all podman processes before"
@@ -47,11 +48,11 @@ parse_command_line_args() {
     exit 1
   fi
 
-  OPT_CHECKOUT_CONTAINERS='1'
+  OPT_CHECKOUT_PODMAN='master'
   OPT_CLEANUP_CONTAINERS='1'
   OPT_KILL_PODMAN='1'
   OPT_MESSAGE=''
-  OPT_PODMAN_VERSION="$1"
+  OPT_SUITE_TAG="${1}"
   shift
   OPT_PYTEST_ARGS=''
 
@@ -62,12 +63,13 @@ parse_command_line_args() {
         usage
         exit 1
         ;;
+      -c|--checkout)
+        readonly OPT_CHECKOUT_PODMAN="$2"
+        shift
+        ;;
       -m|--message)
         readonly OPT_MESSAGE="$2"
         shift
-        ;;
-      --no-checkout)
-        readonly OPT_CHECKOUT_CONTAINERS=''
         ;;
       --no-cleanup)
         readonly OPT_CLEANUP_CONTAINERS=''
@@ -91,13 +93,13 @@ function main() {
   parse_command_line_args "$@"
 
   cd "${PODMAN_REPO_PATH}"
-  if [[ -n "${OPT_CHECKOUT_CONTAINERS}" ]]; then
-    git checkout "${OPT_PODMAN_VERSION}"
+  if [[ -n "${OPT_CHECKOUT_PODMAN}" ]]; then
+    git checkout "${OPT_SUITE_TAG}"
   fi
 
   local PODMAN_COMMIT_ID="$(git log -1 --format=%H)"
   local PODMAN_COMMIT_DATE="$(git log -1 --format=%cI)"
-  local LOG_BASE_NAME="${DOCKER_PY_LOGS_PATH}/pytest_integration_dev_${PODMAN_COMMIT_DATE}_${PODMAN_COMMIT_ID}"
+  local LOG_BASE_NAME="${DOCKER_PY_LOGS_PATH}/pytest_integration_${OPT_SUITE_TAG}_${PODMAN_COMMIT_DATE}_${PODMAN_COMMIT_ID}"
 
   if [[ -n "${OPT_MESSAGE}" ]]; then
     LOG_BASE_NAME="${LOG_BASE_NAME}_${OPT_MESSAGE}"
