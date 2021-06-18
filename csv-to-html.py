441bb4fd355ddab08d1fb21b69ce36d8aab02b4c
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2021-06-18 15:09:55 +0200

    Replace test_names with test_infos grouping some related data for templates

--- csv-to-html.py
+++ csv-to-html.py
@@ -35,12 +35,22 @@ def main():
         exit(1)
 
     tests_by_name_and_suite = defaultdict(lambda: defaultdict(defaultdict))
-    test_names = set()
+    test_infos_by_name = dict()
     test_suites = dict()
     test_summaries_by_suite = defaultdict(lambda: defaultdict(int))
     for test_name, tests in groupby(all_tests, lambda t: t['_test_name']):
         tests = list(tests)
-        test_names.add(test_name)
+
+        test_infos_by_name[test_name] = test_infos_by_name.get(
+            test_name,
+            dict(
+                test_name=test_name,
+                test_file='',
+                test_class='',
+                test_method='',
+                _unsupported=False,
+            )
+        )
 
         for test in tests:
             # fix data format
@@ -60,13 +70,18 @@ def main():
             tests_by_name_and_suite[test_name][test['_test_suite']] = test
             test_summaries_by_suite[test['_test_suite']][test['result']] += 1
 
-    test_names = list(sorted(test_names))
-    test_suites = list((test_suite for (_, test_suite) in sorted(test_suites.items(), reverse=True)))
+            test_infos_by_name[test_name]['test_file'] = test['test_file']
+            test_infos_by_name[test_name]['test_class'] = test['test_class']
+            test_infos_by_name[test_name]['test_method'] = test['test_method']
+            test_infos_by_name[test_name]['_unsupported'] = test_infos_by_name[test_name]['_unsupported'] or test['_unsupported']
+
+    test_infos = list(test_info for (_, test_info) in sorted(test_infos_by_name.items()))
+    test_suites = list(test_suite for (_, test_suite) in sorted(test_suites.items(), reverse=True))
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
     template = env.get_template('template.html.j2')
     print(template.render(
-        test_names=test_names,
+        test_infos=test_infos,
         test_suites=test_suites,
         tests_by_name_and_suite=tests_by_name_and_suite,
         test_summaries_by_suite=test_summaries_by_suite,
